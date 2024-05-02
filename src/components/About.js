6a834c4b0ed392c31798f059b54bf18c87b48756
Joseph <josephshonko99@gmail.com>
2024-05-02 12:05:59 +0300

    commiting hooks props

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from "react";
-
-function About() {
+import Links from "./Links"
+function About(props) {
+ 
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* <p>Put the bio in here</p> */}
+      {props.bio && <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      {props.Links && <Links {...props.Links} />}
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
   );
 }
